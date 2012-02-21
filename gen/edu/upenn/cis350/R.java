cceade84a0b075fdcf011fee3413a4a4bae9ddd1
Nate Close <closen@seas.upenn.edu>
2012-02-21 00:01:43 -0500

    updated event form

--- gen/edu/upenn/cis350/R.java
+++ gen/edu/upenn/cis350/R.java
@@ -14,12 +14,15 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int endDateDisplay=0x7f050004;
-        public static final int eventButton=0x7f050005;
-        public static final int eventTitle=0x7f050000;
-        public static final int pickEndDate=0x7f050003;
-        public static final int pickStartDate=0x7f050001;
-        public static final int startDateDisplay=0x7f050002;
+        public static final int endDateDisplay=0x7f050007;
+        public static final int eventActions=0x7f050003;
+        public static final int eventButton=0x7f050008;
+        public static final int eventDesc=0x7f050002;
+        public static final int eventTitle=0x7f050001;
+        public static final int pickEndDate=0x7f050006;
+        public static final int pickStartDate=0x7f050004;
+        public static final int scroller=0x7f050000;
+        public static final int startDateDisplay=0x7f050005;
     }
     public static final class layout {
         public static final int eventform=0x7f030000;
@@ -28,6 +31,9 @@ public final class R {
     public static final class string {
         public static final int app_name=0x7f040001;
         public static final int createEvent=0x7f040002;
+        public static final int endDate=0x7f040007;
+        public static final int eventActions=0x7f040009;
+        public static final int eventDesc=0x7f040008;
         public static final int eventTitle=0x7f040005;
         public static final int eventWelcome=0x7f040003;
         public static final int hello=0x7f040000;
