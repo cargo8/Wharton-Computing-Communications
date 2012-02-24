6cde76897253631cf1d91e0bfbd95c837b28d9bd
Jason Mow <jason.mow@gmail.com>
2012-02-24 01:15:05 -0500

    Added agenda frame

--- src/edu/upenn/cis350/Agenda.java
+++ src/edu/upenn/cis350/Agenda.java
@@ -4,21 +4,35 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.LinearLayout;
 
 
 public class Agenda extends Activity {
 
+	private LinearLayout[] events;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.agenda);
+        
+        events = new LinearLayout[10];
+        events[0] = (LinearLayout) findViewById(R.id.event1);
+        events[1] = (LinearLayout) findViewById(R.id.event2);
+        events[2] = (LinearLayout) findViewById(R.id.event3);
+        events[3] = (LinearLayout) findViewById(R.id.event4);
+        events[4] = (LinearLayout) findViewById(R.id.event5);
+        events[5] = (LinearLayout) findViewById(R.id.event6);
+        events[6] = (LinearLayout) findViewById(R.id.event7);
+        events[7] = (LinearLayout) findViewById(R.id.event8);
+        events[8] = (LinearLayout) findViewById(R.id.event9);
+        events[9] = (LinearLayout) findViewById(R.id.event10);
+        
     }
     
-    public void onBackToHomeClick(View view){
-    	//TODO closen: transition to main activity
-    	Intent i = new Intent(this, WhartonComputingCommunicationsActivity.class);
+    public void showEvent(View view) {
+    	Intent i = new Intent(this, ShowEvent.class);
     	startActivity(i);
     }
-	
 }
