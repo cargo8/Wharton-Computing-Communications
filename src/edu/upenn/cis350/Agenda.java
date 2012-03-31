46aaa8fa3d79e27498698b41d409320354e1db01
Jason Mow <jason.mow@gmail.com>
2012-03-31 13:28:05 -0400

    Removing Sqlite code from Agenda

--- src/edu/upenn/cis350/Agenda.java
+++ src/edu/upenn/cis350/Agenda.java
@@ -1,7 +1,16 @@
 package edu.upenn.cis350;
 
-import java.util.ArrayList;
 import java.util.List;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.parse.FindCallback;
 import com.parse.Parse;
@@ -9,31 +18,12 @@ import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 
-import android.app.Activity;
-import android.content.ContentValues;
-import android.content.Context;
-import android.content.Intent;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
-import android.graphics.Color;
-import android.graphics.Typeface;
-import android.os.Bundle;
-import android.view.MotionEvent;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.View.OnTouchListener;
-import android.widget.LinearLayout;
-import android.widget.TextView;
-import android.widget.Toast;
-
 
 /* This activity shows the events in a list form.
  * Events are separated by type - Emergency and Scheduled.
  * Clicking on an event goes to the ShowEvent view for that Event.
  */
 public class Agenda extends Activity {
-	
-	private String userKey;
 	
     /** Called when the activity is first created. */
     @Override
@@ -142,71 +132,10 @@ public class Agenda extends Activity {
     	return eventFrame;
     }
     
-    // gets all events from SQLite DB
-    public List<EventPOJO> getEvents(){
-    	List<EventPOJO> eventList = new ArrayList<EventPOJO>();
-    	
-		AndroidOpenDbHelper dbHelper = new AndroidOpenDbHelper(this);
-
-		// Get a readable SQLite database, because we are going to read all scheduled tasks from the DB
-		SQLiteDatabase db = dbHelper.getReadableDatabase();
-		dbHelper.createEventsTable(db);
-    	dbHelper.createMessagesTable(db);
-		
-		//(String table, String[] columns, String selection, String[] selectionArgs, String groupBy, String having, String orderBy)
-		Cursor cursor = db.query(AndroidOpenDbHelper.TABLE_NAME_EVENTS, null, null, null, null, null, null);
-		startManagingCursor(cursor);
-		
-		while (cursor.moveToNext()) {
-			String eventTitle = cursor.getString(cursor.getColumnIndex(AndroidOpenDbHelper.COLUMN_NAME_EVENT_TITLE));
-			String eventDesc = cursor.getString(cursor.getColumnIndex(AndroidOpenDbHelper.COLUMN_NAME_EVENT_DESC));
-			int eventSeverity = cursor.getInt(cursor.getColumnIndex(AndroidOpenDbHelper.COLUMN_NAME_EVENT_SEVERITY));
-
-			String eventStart = cursor.getString(cursor.getColumnIndex(AndroidOpenDbHelper.COLUMN_NAME_EVENT_START));
-			String eventEnd = cursor.getString(cursor.getColumnIndex(AndroidOpenDbHelper.COLUMN_NAME_EVENT_END));
-			
-			String eventContact1 = cursor.getString(cursor.getColumnIndex(AndroidOpenDbHelper.COLUMN_NAME_EVENT_CONTACT1));
-			String eventContact2 = cursor.getString(cursor.getColumnIndex(AndroidOpenDbHelper.COLUMN_NAME_EVENT_CONTACT2));
-
-			String eventActions = cursor.getString(cursor.getColumnIndex(AndroidOpenDbHelper.COLUMN_NAME_EVENT_ACTIONS));
-			String eventAffils = cursor.getString(cursor.getColumnIndex(AndroidOpenDbHelper.COLUMN_NAME_EVENT_AFFILS));
-			String eventSystems = cursor.getString(cursor.getColumnIndex(AndroidOpenDbHelper.COLUMN_NAME_EVENT_SYSTEMS));
-			String eventType = cursor.getString(cursor.getColumnIndex(AndroidOpenDbHelper.COLUMN_NAME_EVENT_TYPE));
-			int eventID = cursor.getInt(cursor.getColumnIndex(AndroidOpenDbHelper.COLUMN_NAME_EVENT_ID));
-			
-			EventPOJO event = new EventPOJO();
-			event.setEventTitle(eventTitle);
-			event.setEventDesc(eventDesc);
-			event.setSeverity(eventSeverity);
-			event.setStart(eventStart);
-			event.setEnd(eventEnd);
-			event.setContact1(eventContact1);
-			event.setContact2(eventContact2);
-			event.setEventActions(eventActions);
-			event.setType(eventType);
-			event.setEventID(eventID);
-			String[] affils = eventAffils.split("\t");
-			for (String s : affils) {
-				if(!s.equals(""))
-					event.addToAffils(s);
-			}
-			String[] systems = eventSystems.split("\t");
-			for (String s : systems) {
-				if(!s.equals(""))
-					event.addToSystems(s);
-			}
-			
-			eventList.add(event);
-		}
-		
-		db.close();
-		
-//		Toast.makeText(this, "Values inserted column ID is :" + affectedColumnId, Toast.LENGTH_SHORT).show();
-		return eventList;
-	}
-    
     @Override
-    // changes behavior when back button is pressed
+    /**
+     * Returns to Home screen on back press no matter what
+     */
     public void onBackPressed() {
        Intent i = new Intent(this, Home.class);
        startActivity(i);
