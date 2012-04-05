9ab5795c223d4c45d8b5db34d998fd810c93c15d
Jason Mow <jason.mow@gmail.com>
2012-04-05 17:55:22 -0400

    Changed edit user to use saveEventually()

--- src/edu/upenn/cis350/EditProfile.java
+++ src/edu/upenn/cis350/EditProfile.java
@@ -8,9 +8,7 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.parse.Parse;
-import com.parse.ParseException;
 import com.parse.ParseUser;
-import com.parse.SaveCallback;
 
 /* This activity displays a form for the user to register a user name for the app.
  * In the future, this will contain more fields to create a more robust "contact"
@@ -83,21 +81,8 @@ public class EditProfile extends Activity {
     	user.put("phone1", phone1);
     	user.put("phone2", phone2);
 
-    	final Toast successToast = Toast.makeText(this, "User " + uname + " updated.", Toast.LENGTH_SHORT);
-		final Intent i = new Intent(this, Login.class);
-		
-		final Toast failToast = Toast.makeText(this, "Could not update user. Try again.", Toast.LENGTH_SHORT);
-		
-    	user.saveInBackground(new SaveCallback() {
-    	    public void done(ParseException e) {
-    	        if (e == null) {
-					successToast.show();
-		    		startActivity(i);
-    	    	} else {
-					failToast.show();
-					return;
-    	        }
-    	    }
-    	});	
+		user.saveEventually();
+		Intent i = new Intent(this, Login.class);
+		startActivity(i);
     }
 }
