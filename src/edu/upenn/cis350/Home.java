6263c44d7404797bc89790402d79b1ef94c40280
Jason Mow <jason.mow@gmail.com>
2012-04-05 16:49:42 -0400

    Changing home to list view

--- src/edu/upenn/cis350/Home.java
+++ src/edu/upenn/cis350/Home.java
@@ -1,9 +1,16 @@
 package edu.upenn.cis350;
 
 import android.app.Activity;
+import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.parse.Parse;
 import com.parse.ParseUser;
@@ -11,31 +18,48 @@ import com.parse.ParseUser;
 /* This activity is what the app will start with after logging in.
  * For now, it just shows a few buttons to create a new event
  * and to show the agenda.
- * TODO: Turn into a ListView
  */
-public class Home extends Activity {
+public class Home extends ListActivity {
 	
-	/** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-		Parse.initialize(this, "FWyFNrvpkliSb7nBNugCNttN5HWpcbfaOWEutejH", "SZoWtHw28U44nJy8uKtV2oAQ8suuCZnFLklFSk46");
-        setContentView(R.layout.home);
-    }
-    
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	  super.onCreate(savedInstanceState);
+
+	  String[] options = getResources().getStringArray(R.array.home_options_array);
+	  setListAdapter(new ArrayAdapter<String>(this, R.layout.home_list_item, options));
+
+	  ListView lv = getListView();
+	  lv.setTextFilterEnabled(true);
+
+	  lv.setOnItemClickListener(new OnItemClickListener() {
+	    public void onItemClick(AdapterView<?> parent, View view,
+	        int position, long id) {
+	    	String label = ((TextView) view).getText().toString();
+	    	if ("Create New Event".equals(label)) {
+	    		onCreateEventClick();
+	    	} else if ("Show Agenda".equals(label)) {
+	    		onShowAgenda();
+	    	} else if ("Show Contacts".equals(label)) {
+	    		onShowContacts();
+	    	}
+	      }
+	    }
+	  );
+	}
+	
     // onClick function for createEvent button
-    public void onCreateEventClick(View view){
+    public void onCreateEventClick(){
     	Intent i = new Intent(this, CreateNewEvent.class);
     	startActivity(i);
     }
     
     // onClick function for showAgenda button
-    public void onShowAgenda(View view) {
+    public void onShowAgenda() {
     	Intent i = new Intent(this, Agenda.class);
     	startActivity(i);
     }
     
-    public void onShowContacts(View view) {
+    public void onShowContacts() {
     	Intent i = new Intent(this, ContactList.class);
     	startActivity(i);
     }
